5b42a2d77cbffebe52cc3ea95e7e0e7a4b937477
junghyeonsu <jung660317@naver.com>
2023-12-12 21:06:57 +0900

    chore: vanilla-extract 설치 및 메인 페이지 수정

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,49 +1,49 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 const config: Config = {
-  title: '글또 Document',
-  favicon: 'img/favicon.ico',
+  title: "글또 Document",
+  favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: 'https://geultto.github.io',
-  baseUrl: '/',
+  url: "https://geultto.github.io",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
-  projectName: 'geultto.github.io',
-  organizationName: 'geultto',
+  projectName: "geultto.github.io",
+  organizationName: "geultto",
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
   trailingSlash: true,
-  deploymentBranch: 'gh-pages',
+  deploymentBranch: "gh-pages",
 
   i18n: {
-    defaultLocale: 'ko',
-    locales: ['ko'],
+    defaultLocale: "ko",
+    locales: ["ko"],
   },
+
+  plugins: ["docusaurus-plugin-vanilla-extract"],
 
   presets: [
     [
-      'classic',
+      "classic",
       {
         docs: {
-          sidebarPath: './sidebars.ts',
+          sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/geultto/geultto.github.io/tree/main/docs',
+          editUrl: "https://github.com/geultto/geultto.github.io/tree/main/docs",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/geultto/geultto.github.io/tree/main/blog',
+          editUrl: "https://github.com/geultto/geultto.github.io/tree/main/blog",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       } satisfies Preset.Options,
     ],
@@ -51,63 +51,63 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/geultto-social-card.png',
+    image: "img/geultto-social-card.png",
     navbar: {
-      title: '글또',
+      title: "글또",
       logo: {
-        alt: '글또 Logo',
-        src: 'img/logo.svg',
+        alt: "글또 Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          type: 'docSidebar',
-          sidebarId: 'tutorialSidebar',
-          position: 'left',
-          label: 'Tutorial',
+          type: "docSidebar",
+          sidebarId: "tutorialSidebar",
+          position: "left",
+          label: "Tutorial",
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
+        { to: "/blog", label: "Blog", position: "left" },
         {
-          href: 'https://github.com/geultto/geultto.github.io',
-          label: 'GitHub',
-          position: 'right',
+          href: "https://github.com/geultto/geultto.github.io",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: '튜토리얼',
-              to: '/docs/intro',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: '글또 Organization',
-              href: 'https://github.com/geultto/',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: '블로그',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/geultto/geultto.github.io',
-            },
-          ],
-        },
-      ],
+      style: "light",
+      // links: [
+      //   {
+      //     title: "Docs",
+      //     items: [
+      //       {
+      //         label: "튜토리얼",
+      //         to: "/docs/intro",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "Community",
+      //     items: [
+      //       {
+      //         label: "글또 Organization",
+      //         href: "https://github.com/geultto/",
+      //       },
+      //     ],
+      //   },
+      //   {
+      //     title: "More",
+      //     items: [
+      //       {
+      //         label: "블로그",
+      //         to: "/blog",
+      //       },
+      //       {
+      //         label: "GitHub",
+      //         href: "https://github.com/geultto/geultto.github.io",
+      //       },
+      //     ],
+      //   },
+      // ],
       copyright: `Copyright © ${new Date().getFullYear()} 글또`,
     },
     prism: {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,21 @@
-import clsx from 'clsx';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import Heading from '@theme/Heading';
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import Layout from "@theme/Layout";
+import Heading from "@theme/Heading";
 
 import Logo from "../../static/img/logo.svg";
 
-import styles from './index.module.css';
+import * as styles from "./index.css";
 
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
+export default function Home() {
+  const { siteConfig } = useDocusaurusContext();
+
   return (
-    <header className={styles.heroBanner}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          글또 Document
-        </Heading>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
+    <Layout title={siteConfig.title} description="글또 Document">
+      <main>
+        <div className={styles.container}>
+          <Heading as="h1">글또 Document</Heading>
           <Logo />
         </div>
-      </div>
-    </header>
-  );
-}
-
-export default function Home(): JSX.Element {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
       </main>
     </Layout>
   );
