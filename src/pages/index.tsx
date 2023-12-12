2ced0feec404683428563f1b674dfe4e722e54f6
junghyeonsu <jung660317@naver.com>
2023-12-12 21:32:54 +0900

    chore: 홈 title 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,13 @@
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Layout from "@theme/Layout";
 import Heading from "@theme/Heading";
+import Layout from "@theme/Layout";
 
 import Logo from "../../static/img/logo.svg";
 
 import styles from "./index.module.css";
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-
-  console.log("styles", styles);
-
   return (
-    <Layout title={siteConfig.title} description="글또 Document">
+    <Layout title="홈" description="글또 Document">
       <main>
         <div className={styles.container}>
           <Heading as="h1">글또 Document</Heading>
