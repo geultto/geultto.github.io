de2235a73994d56c6c880ee4d989c9bc0730ecef
KumJungMin <rmawjdals@naver.com>
2024-12-19 20:38:40 +0900

    feat: 인터뷰어 프로필 카드용 ProfileCard 컴포넌트 추가

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,24 @@
+import React, { type ReactNode } from "react";
+import styles from "./ProfileCard.module.css";
+
+interface ProfileCardProps {
+  profileImg: string;
+  title: string;
+  url: string;
+  children: ReactNode;
+}
+
+export const ProfileCard = (props: ProfileCardProps) => {
+  const { profileImg, title, url, children } = props;
+
+  return (
+    <a className={styles.profileCard} href={url}>
+      <img src={profileImg} alt="Profile" />
+      <div className={styles.content}>
+        <h3>{title}</h3>
+        {children}
+        <p>{url}</p>
+      </div>
+    </a>
+  );
+};
