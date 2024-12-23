9176130d1fccd7377bd489a56d4a15e9a4ec5fe3
KumJungMin <37934668+KumJungMin@users.noreply.github.com>
2024-12-23 21:16:00 +0900

    docs: 오픈소스 인터뷰 시리즈 첫번째 포스팅 추가하기 (#35)
    * feat: divider 컴포넌트 추가
    
    * feat: 인터뷰 답변용 Answer 컴포넌트 추가
    
    * feat: 인터뷰 질문용 Question 컴포넌트 추가
    
    * feat: 인터뷰어 프로필 카드용 ProfileCard 컴포넌트 추가
    
    * feat: 추가한 컴포넌트 등록하기
    
    * docs: 오픈소스 인터뷰 시리즈 첫번째 포스팅 추가하기
    
    * docs: 띄어쓰기 반영하기
    
    * docs: 답변 방향 일치시키기
    
    * docs: 인터뷰 부가 설명을 할 수 있는 이미지 추가
    
    * docs: 마지막 멘트 수정
    
    * style: 가독성을 위해 질문 간격 늘리기
    
    * docs: 시리즈 인터뷰 명 수 언급하기
    
    * feat: 툴팁 추가 및 단어 설명란 추가하기
    
    * refactor: 툴팁이 화면을 벗어나지 않도록 수정하기
    
    * docs: 메인테이너 단어 일치시키기
    
    * docs: MDN 문서 링크 추가하기

---
+++ src/components/Answer.tsx
@@ -0,0 +1,30 @@
+import React, { type ReactNode } from "react";
+import clsx from "clsx";
+
+import styles from "./Answer.module.css";
+
+interface AnswerProps {
+  children: ReactNode;
+  profileImg?: string;
+  name?: string;
+  direction?: "left" | "right" | "center";
+}
+
+export const Answer = (props: AnswerProps) => {
+  const { children, profileImg, name = "", direction = "right" } = props;
+
+  return (
+    <div className={clsx(styles.container, styles[direction])}>
+      {direction === "left" && <div className={styles.content}>{children}</div>}
+      {profileImg && (
+        <div className={styles.profileContainer}>
+          <div className={styles.profile}>
+            <img src={profileImg} alt="profile" />
+          </div>
+          {name && <p className={styles.name}>{name}</p>}
+        </div>
+      )}
+      {(direction === "right" || direction === "center") && <div className={styles.content}>{children}</div>}
+    </div>
+  );
+};
--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -1,7 +1,17 @@
 import MDXComponents from "@theme-original/MDXComponents";
 import { Curation } from "../components/Curation";
+import { Question } from "../components/Question";
+import { Answer } from "../components/Answer";
+import { Divider } from "../components/Divider";
+import { ProfileCard } from "../components/ProfileCard";
+import { HintTooltip } from "../components/HintTooltip";
 
 export default {
   ...MDXComponents,
   Curation,
+  Question,
+  Answer,
+  Divider,
+  ProfileCard,
+  HintTooltip,
 };
