1587d4ac2fa7ae4d094497bc5e52cdf7c7f5cacd
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2025-01-11 23:35:58 +0900

    feat: 태그 추가

--- app/blog/[[...slug]]/page.tsx
+++ app/blog/[[...slug]]/page.tsx
@@ -1,12 +1,13 @@
 import { mdxComponents } from "@/src/components/mdx-components";
 import { blogSource } from "@/src/lib/source";
-import { MDXContent } from '@content-collections/mdx/react';
+import { MDXContent } from "@content-collections/mdx/react";
 import { DocsBody, DocsDescription, DocsPage, DocsTitle } from "fumadocs-ui/page";
 import { notFound } from "next/navigation";
 
 import type { Metadata } from "next";
 import type { MDXComponents } from "mdx/types";
 import Author from "@/src/components/Author";
+import { Badge } from "@/src/components/ui/badge";
 
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export default async function Page(props: any) {
@@ -20,20 +21,25 @@ export default async function Page(props: any) {
     <DocsPage toc={page.data.toc} full={page.data.full}>
       <DocsTitle>{page.data.title}</DocsTitle>
       <DocsDescription>{page.data.description}</DocsDescription>
-      {page.data.updatedAt ?
-        <span className="text-sm text-gray-500">해당 글은 {page.data.updatedAt}에 수정되었습니다.</span> :
-        page.data.createdAt ? <span className="text-sm text-gray-500">해당 글은 {page.data.createdAt}에 작성되었습니다.</span> : null
-      }
+      <div className="flex items-center gap-2">
+        {page.data.updatedAt ? (
+          <span className="text-sm text-gray-500">{page.data.updatedAt} updated</span>
+        ) : page.data.createdAt ? (
+          <span className="text-sm text-gray-500">{page.data.createdAt}</span>
+        ) : null}
+        {page.data.tags?.map((tag) => (
+          <Badge variant="secondary" key={tag}>
+            {tag}
+          </Badge>
+        ))}
+      </div>
       <div className="flex gap-4">
         {authors.map((author) => (
           <Author key={author} name={author} />
         ))}
       </div>
       <DocsBody>
-        <MDXContent
-          code={page.data.body}
-          components={{ ...mdxComponents as MDXComponents }}
-        />
+        <MDXContent code={page.data.body} components={{ ...(mdxComponents as MDXComponents) }} />
       </DocsBody>
     </DocsPage>
   );
